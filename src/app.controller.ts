0cfd81c42422ecde3bf40a1e7e3cd8e0270a7f90
yasir-tella12 <yasir.tella@pau.edu.ng>
2024-07-09 11:57:34 +0100

    second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+// app.controller.ts
+
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('hello')
+  @Render('index.html')
+  getHello(): {} {
+    return this.appService.getHello2();
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home.html')
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+
+  @Get('about-us')
+  @Render('about-us.html')
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Get the underlying Express instance
+  const express = app.getHttpAdapter().getInstance();
+
+  // Define the directory containing your template files (views)
+  const views = join(__dirname, '..', 'views');
+
+  // Configure Nunjucks with Express
+  nunjucks.configure(views, {
+    express: express,
+    autoescape: true,  // Optional: Enable autoescaping of HTML characters
+    noCache: true      // Optional: Disable caching of templates (for development)
+  });
+
+  // Serve static assets from the 'static' folder
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
+
+  // Set the 'views' directory for template rendering
+  app.setBaseViewsDir(views);
+
+  // Start the Nest application
   await app.listen(3000);
 }
+
 bootstrap();
